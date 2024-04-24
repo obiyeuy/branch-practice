82cefb2200374064116b96abad6a65a3dac91d16
obiyeuy <yueyibohitwh@126.com>
2024-04-24 09:29:09 +0800

    B1第一次提交

---
+++ DishPageQueryDTO.java
@@ -0,0 +1,23 @@
+package com.sky.dto;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class DishPageQueryDTO implements Serializable {
+
+    private int page;
+
+    private int pageSize;
+
+    //菜品名称
+    private String name;
+
+    //分类id
+    private Integer categoryId;
+
+    //状态 0表示禁用 1表示启用
+    private Integer status;
+
+}
